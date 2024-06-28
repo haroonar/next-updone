bc94f6c2ccd5100b0676ab0eb12c0f2ad8b5c5ec
haroonarif <haroonarif@cosmoinc.co>
2024-06-28 09:07:21 +0500

    updated code

--- app/components/common/Calander.tsx
+++ app/components/common/Calander.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 import './CalendarStyles.css'; // Import custom CSS file
@@ -19,7 +19,19 @@ const CalendarWithAvailability: React.FC<CalendarWithAvailabilityProps> = ({ isC
     setDate(value);
   };
 
+  const scrollRef = useRef(null);
 
+  const scrollUp = () => {
+    if (scrollRef.current) {
+      scrollRef.current.scrollBy({ top: -100, behavior: 'smooth' });
+    }
+  };
+
+  const scrollDown = () => {
+    if (scrollRef.current) {
+      scrollRef.current.scrollBy({ top: 100, behavior: 'smooth' });
+    }
+  };
   // Find the time zone identifier for US Eastern Time
   const usEasternTimeZone = "America/New_York";
 
@@ -47,19 +59,61 @@ const CalendarWithAvailability: React.FC<CalendarWithAvailabilityProps> = ({ isC
           </div>
         </div>
       </div>
-      <div style={{ width: "35%", margin: 'auto' }}>
-        <h2 className="invisible">Sarah's Availability</h2>
-        <div className="space-y-2 p-2" style={{ maxHeight: '300px', overflowY: 'auto' }}>
-          {TIMES_DATA.map((time, index) => (
-            <div
-              key={index}
-              className={` px-[10px] py-[12px]  rounded-lg ${index === 2 ? 'selected-style text-white' : 'text-[#350ABC]'}  bg-[#FAF9FF] text-center`}
-            >
-              {time}
-            </div>
-          ))}
-        </div>
+      <div style={{ width: '35%', margin: 'auto', position: 'relative' }}>
+      <h2 className="invisible">Sarah's Availability</h2>
+      <div
+        ref={scrollRef}
+        className="space-y-2 p-2"
+        style={{ maxHeight: '300px', overflow: 'hidden' }}
+      >
+        {TIMES_DATA.map((time, index) => (
+          <div
+            key={index}
+            className={`px-[10px] py-[12px] rounded-lg ${index === 2 ? 'selected-style text-white' : 'text-[#350ABC]'} bg-[#FAF9FF] text-center`}
+          >
+            {time}
+          </div>
+        ))}
       </div>
+      <button
+        onClick={scrollUp}
+        style={{
+          position: 'absolute',
+          top: '86px',
+          right: '-12px',
+          backgroundColor: '#FAF9FF',
+          border: 'none',
+          borderRadius: '50%',
+          cursor: 'pointer',
+        }}
+      >
+<svg width="8" height="47" viewBox="0 0 8 47" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M3.64645 0.146446C3.84171 -0.0488167 4.15829 -0.0488167 4.35356 0.146446L7.53554 3.32843C7.7308 3.52369 7.7308 3.84027 7.53554 4.03553C7.34027 4.23079 7.02369 4.23079 6.82843 4.03553L4 1.2071L1.17157 4.03553C0.976313 4.23079 0.65973 4.23079 0.464468 4.03553C0.269206 3.84027 0.269206 3.52369 0.464468 3.32843L3.64645 0.146446ZM3.5 46.5L3.5 0.5L4.5 0.5L4.5 46.5L3.5 46.5Z" fill="#D6D4D4"/>
+</svg>
+
+
+
+      </button>
+      <button
+        onClick={scrollDown}
+        style={{
+          position: 'absolute',
+          bottom: '35px',
+          right: '-12px',
+          backgroundColor: '#FAF9FF',
+          border: 'none',
+          borderRadius: '50%',
+          cursor: 'pointer',
+        }}
+      >
+        <svg width="8" height="47" viewBox="0 0 8 47" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M3.64645 46.8536C3.84171 47.0488 4.15829 47.0488 4.35356 46.8536L7.53554 43.6716C7.7308 43.4763 7.7308 43.1597 7.53554 42.9645C7.34027 42.7692 7.02369 42.7692 6.82843 42.9645L4 45.7929L1.17157 42.9645C0.976313 42.7692 0.65973 42.7692 0.464468 42.9645C0.269206 43.1597 0.269206 43.4763 0.464468 43.6716L3.64645 46.8536ZM3.5 0.5L3.5 46.5L4.5 46.5L4.5 0.5L3.5 0.5Z" fill="#D6D4D4"/>
+</svg>
+
+
+
+      </button>
+    </div>
     </div>
   );
 };
