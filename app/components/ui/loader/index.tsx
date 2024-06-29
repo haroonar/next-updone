01752daf080fcfb5a9b364bcc7a31b1f4a1eea2d
haroonarif <haroonarif@cosmoinc.co>
2024-06-30 02:39:52 +0500

    updated

--- app/components/ui/loader/index.tsx
+++ app/components/ui/loader/index.tsx
@@ -2,19 +2,19 @@ import Image from "next/image";
 
 const RenderLoader = () => {
     return (
-       <div className="h-screen w-full flex justify-center items-center">
+       <div className="h-[80vh] w-full flex justify-center items-center">
          <div className='grid grid-cols-2 items-center justify-center h-26 w-26 mx-auto'>
             <div className="flex items-center justify-center">
-                <Image src='/images/gallery/bartender.gif' width={100} className="relative left-1 top-2" height={100} alt='Loader 1' />
+                <Image src='/images/gallery/bartender.gif' width={80} className="relative left-1 top-2" height={80} alt='Loader 1' />
             </div>
             <div className="flex items-center justify-center">
-                <Image src='/images/gallery/food-delivery.gif' className="relative top-2" width={100} height={100} alt='Loader 2' />
+                <Image src='/images/gallery/food-delivery.gif' className="relative top-2" width={80} height={80} alt='Loader 2' />
             </div>
             <div className="flex items-center justify-center">
-                <Image src='/images/gallery/bar-counter.gif' width={100} height={100} alt='Loader 3' />
+                <Image src='/images/gallery/bar-counter.gif' width={80} height={80} alt='Loader 3' />
             </div>
             <div className="flex items-center justify-center">
-                <Image src='/images/gallery/beer-tap.gif' width={100} height={100} alt='Loader 4' />
+                <Image src='/images/gallery/beer-tap.gif' width={80} height={100} alt='Loader 4' />
             </div>
         </div>
        </div>
--- app/staff/detail/page.tsx
+++ app/staff/detail/page.tsx
@@ -1,7 +1,11 @@
 'use client'
-import StaffDetail from '@/app/components/home/detail';
+import Loader from '@/app/components/ui/loader';
+import dynamic from 'next/dynamic';
 import React from 'react';
-
+const StaffDetail = dynamic(() => import('@/app/components/home/detail'), {
+  loading: () => <Loader />, // Display loader while loading
+  ssr: false, // Do not SSR for this component
+});
 const page = () => {
   //this is to get data from store 
   return (
