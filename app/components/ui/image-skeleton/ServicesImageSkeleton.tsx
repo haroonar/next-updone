288ebaa335b3ae3e73c492347240a32edb9636dc
haroonarif <haroonarif@cosmoinc.co>
2024-07-12 19:39:21 +0500

    updated

--- app/components/ui/image-skeleton/ServicesImageSkeleton.tsx
+++ app/components/ui/image-skeleton/ServicesImageSkeleton.tsx
@@ -11,23 +11,24 @@ interface ServiceImageWithSkeletonProps {
   width?: number;
   height?: number;
   isHeroImg?:boolean;
+  setIsServiceLoaded:any
+  isServiceLoaded:boolean
 }
 
-const ServiceImageWithSkeleton: React.FC<ServiceImageWithSkeletonProps> = ({ src, alt, width, height,isHeroImg }) => {
-  const [isLoaded, setIsLoaded] = useState(false);
+const ServiceImageWithSkeleton: React.FC<ServiceImageWithSkeletonProps> = ({ src, alt, width, height,isServiceLoaded ,setIsServiceLoaded}) => {
 
   return (
     <>
    
     <div style={ { height}} >
-      {!isLoaded && <Skeleton width={width} height={height}  className='animate-pulse bg-gray-50' />}
+      {!isServiceLoaded && <Skeleton width={width} height={height}  className='animate-pulse bg-gray-50' />}
       <Image
        layout="responsive"  quality={100} 
         src={src}
         alt={alt}
         width={width}
         height={height}
-        onLoad={() => setIsLoaded(true)}
+        onLoad={() => setIsServiceLoaded(true)}
         className='shadow-stone-300'
         
         // style={{ display: isLoaded ? 'block' : 'none', position: 'absolute', top: 0, left: 0 }}
